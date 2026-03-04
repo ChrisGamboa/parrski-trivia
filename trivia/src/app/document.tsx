5d96015ac988c7758456dabe6bcce2af7209ab69
Christian Gamboa <chris@goprzm.com>
2026-03-04 00:11:05 -0500

    Use Pickles photo as favicon

--- trivia/src/app/document.tsx
+++ trivia/src/app/document.tsx
@@ -7,6 +7,7 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" type="image/png" href="/images/pickles.png" />
       <title>Parrski Trivia</title>
       <link rel="stylesheet" href={boomerCssUrl} />
       <link rel="modulepreload" href="/src/client.tsx" />
